e69eda3af868788028503d6878417fe4f79839d9
Wanski <euanjaranilla3@gmail.com>
2026-04-22 15:25:55 +0800

    first commit

---
+++ Product10Seperation/main.py
@@ -0,0 +1,8 @@
+from ElectronicProduct import ElectronicProduct
+from FoodProduct import FruitProduct
+
+
+product1 = ElectronicProduct(101, "Laptop", 45000, 10, 2)
+product2 = FruitProduct(102, "Apple" , 25 , 5 , "October 6, 2027"  , "Yes" , 90 )
+product1.display_product()
+product2.display_product()
